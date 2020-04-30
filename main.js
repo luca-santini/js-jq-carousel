ff9565b9ef20a4f6d3760d01adc0cdcbcf824462
Luca Santini <lucasantinidev@gmail.com>
2020-04-30 17:56:43 +0200

    slider stile css

---
+++ main.js
@@ -0,0 +1,9 @@
+// pulsante next: quando l'utente clicca, devo spostyare la classe active all'img successiva
+
+// intercettare il click sulla classe next
+
+// recupera l'immagine che ha la classe active
+
+// tolgo la classe avtive all'img corrente
+
+// metto la classe active all'img successiva
